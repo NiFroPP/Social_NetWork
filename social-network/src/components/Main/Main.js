a6c9290fc4fca7235290b31546bafb2660929ae1
Nick Frolov <frolovnick0@gmail.com>
2021-08-02 18:41:46 +0300

    feat: add main-component

--- social-network/src/components/Main/Main.js
+++ social-network/src/components/Main/Main.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Main.scss";
+import NavBar from "./NavBar/NavBar";
+import MainContent from "./MainContent/MainContent";
 
 const Main = () => {
-  return <main className="main">MAIN</main>;
+  return (
+    <main className="main">
+      <NavBar />
+      <MainContent />
+    </main>
+  );
 };
 
 export default Main;
---
+++ social-network/src/components/Main/MainContent/MainContent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MainContent.scss";
+
+const MainContent = () => {
+  return <div className="main__content">MainContent</div>;
+};
+
+export default MainContent;
